c16f09b01587e48d383704ff7b47e06efdd13351
Minh Bui <tranduyminh.bui@gmail.com>
2020-04-14 14:35:47 +1000

    Added initial test for Congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Congrats(props) {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>You guessed the word!</span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats'></div>;
+  }
+}
+
+export default Congrats;
