48caa938d2ce78a1127bd23815a55ec87906cb6b
Minh Bui <tranduyminh.bui@gmail.com>
2020-04-16 15:13:43 +1000

    write tests for successReducers

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,1 +1,10 @@
-export const success = () => {};
+import { actionTypes } from '../actions/index';
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
+};
