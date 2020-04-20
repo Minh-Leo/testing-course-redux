62a0a8af26b0ebac6d97fae6bb7bfda19e9288a1
Minh Bui <tranduyminh.bui@gmail.com>
2020-04-20 13:17:45 +1000

    added reducer and actions for guessedWords

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,8 +1,11 @@
 import { correctGuess, actionTypes } from './index';
 
+// describe('correct guess', () => {
+//   test('returns an action with type `CORRECT GUESS`', () => {
+//     const action = correctGuess();
+//     expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+//   });
+// });
 describe('correct guess', () => {
-  test('returns an action with type `CORRECT GUESS`', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
+  test('returns an action with type `CORRECT GUESS`', () => {});
 });
